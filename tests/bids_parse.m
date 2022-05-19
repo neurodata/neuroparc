60c6888f9e3761ea822a07d839f2d9a49c138018
Remi Gau <remi_gau@hotmail.com>
2022-05-19 19:22:15 +0200

    add rudimentary script to validate BIDSiness of filenames

--- tests/bids_parse.m
+++ tests/bids_parse.m
@@ -4,16 +4,29 @@ this_dir = fileparts(mfilename('fullpath'));
 
 label_folder = fullfile(this_dir, '..', 'atlases', 'label', 'Human');
 
-files = bids.internal.file_utils('FPList', label_folder, '^*.nii.gz');
+reference_folder = fullfile(this_dir, '..', 'atlases', 'reference_brains');
 
-for i = 1:size(files, 1)
-  
-  this_file = deblank(files(i, :));
-  
-  try
-    bids.File(this_file, 'tolerant', false, 'verbose', true);
-  catch
-    fprintf(1, '%s is not BIDS friendly\n', bids.internal.file_utils(this_file, 'filename'));
-  end
-  
+mask_folder = fullfile(this_dir, '..', 'atlases', 'mask');
+
+test_folder(label_folder);
+test_folder(reference_folder);
+test_folder(mask_folder);
+
+%%
+
+function test_folder(folder)
+    files = bids.internal.file_utils('FPList', folder, '^*.nii.gz');
+
+    for i = 1:size(files, 1)
+
+        this_file = deblank(files(i, :));
+
+        try
+            bids.File(this_file, 'tolerant', false, 'verbose', true);
+        catch
+            fprintf(1, '%s is not BIDS friendly\n', ...
+                    bids.internal.file_utils(this_file, 'filename'));
+        end
+
+    end
 end

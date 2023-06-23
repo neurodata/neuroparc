44f1b19052d725afd3789177b7dae3e7260b8cfe
Remi Gau <remi_gau@hotmail.com>
2023-06-23 21:52:36 +0200

    prepare script to clean up json

--- clean_up_json.py
+++ clean_up_json.py
@@ -14,5 +14,29 @@ json_files = output_dir.glob("*.json")
 for file in json_files:
 
     print(file)
+
+    resolution = [x.split("-")[1] for x in file.stem.split("_") if x.startswith("res")][0]
+
     with open(file) as f:
-        json_content = json.load(f)
+        content = json.load(f)
+
+    for key in content["MetaData"]:
+        if key == "AtlasName":
+            new_key = "Name"
+        if key == "Source":
+            new_key = "ReferencesAndLinks"
+        else:
+            new_key = "".join([x.capitalize() for x in key.split(" ")])
+
+        if new_key == "ReferencesAndLinks":
+            content[new_key] = [content["MetaData"][key]]
+
+        content[new_key] = content["MetaData"][key]
+    
+    content["Space"] = "MNI152NLin6"
+    content["BIDSVersion"] = "1.8.0"
+    content["Authors"] = ["", ""]
+    content["Species"] = "Homo sapiens"
+    content["Resolution"] = f"[{resolution}, {resolution}, {resolution}] mm"
+
+    print(content)
